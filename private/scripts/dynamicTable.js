06cbbfd68aa0f72feba458d8d82093418276e6d3
gl0ryus <gl0ryus@pm.me>
2022-11-28 23:52:50 -0500

    Adjust ratio colors

--- private/scripts/dynamicTable.js
+++ private/scripts/dynamicTable.js
@@ -1128,25 +1128,24 @@ window.qBittorrent.DynamicTable = (function() {
                         // td.setStyle('color', '#40b82e');
                         td.set('class', 'RatioUncommon');
                         break;
-                    case (ratio < 4.0):
+                    case (ratio < 5.0):
                         // console.log('BEST');
                         // td.setStyle('color', '#40b82e');
                         td.set('class', 'RatioRare');
                         break;
-                    case (ratio < 5.0):
+                    case (ratio < 10.0):
                         // td.setStyle('color', '#eb3434');
                         td.set('class', 'RatioEpic');
                         break;
-                    
-                    case (ratio < 6.0):
+                    case (ratio < 20.0):
                         // td.setStyle('color', '#eb3434');
                         td.set('class', 'RatioLegendary');
                         break;
-                    case (ratio > 6.0):
+                    case (ratio > 40.0):
                         // td.setStyle('color', '#eb3434');
                         td.set('class', 'RatioEpic');
                         break;
-                    case (ratio >= 10.0):
+                    case (ratio >= 50.0):
                         // td.setStyle('color', '#eb3434');
                         td.set('class', 'RatioHerloom');
                         break;
